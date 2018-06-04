10d4d888a805f86cb043feac63b7d220b3d8dbb3
Uthaeus <romanlavery@gmail.com>
2018-06-04 12:20:33 -0600

    Removed first item from archive component

--- src/reducers/newsletterReducer.js
+++ src/reducers/newsletterReducer.js
@@ -4,7 +4,14 @@ import { FETCH_NEWSLETTER_ARCHIVE } from "../actions/types";
 export default function(state = {}, action) {
   switch(action.type) {
     case FETCH_NEWSLETTER_ARCHIVE:
-      return { ...state, archive: action.payload, latestItem: action.payload[0] }
+      const latestItem = action.payload[0];
+      const archive = action.payload;
+      archive.shift();
+      return { 
+        ...state, 
+        archive, 
+        latestItem
+      }
     default: return state;
   }
 }
