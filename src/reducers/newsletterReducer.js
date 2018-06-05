320b091e49f05717d9798cf5f5262f87195107f4
Uthaeus <romanlavery@gmail.com>
2018-06-05 10:54:21 -0600

    Set up initial state in newsletter reducer

--- src/reducers/newsletterReducer.js
+++ src/reducers/newsletterReducer.js
@@ -3,8 +3,14 @@ import {
   FETCH_NEWSLETTER_BY_ID
 } from "../actions/types";
 
+const INITIAL_STATE = {
+  archive: [],
+  latestItem: {},
+  fetchedItem: {}
+}
 
-export default function(state = {}, action) {
+
+export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
     case FETCH_NEWSLETTER_ARCHIVE:
       const latestItem = action.payload[0];
